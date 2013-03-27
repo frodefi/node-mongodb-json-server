6159407e85a95ce00f35a56120a2fc383bef6d21
Frode Fikke <frodefi@gmail.com>
2013-03-27 17:03:14 +0200

    Various test-related changes

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ Testing setting up a RESTful JSON server with Node, Express, Passport authentica
 This server store notes.
 
 Express could (hopefully) very easy be replaced with Restify. But I use for now Express for simplicity
-since all the tuturials out there is refering to Express. I might change that later.
+since most of the tuturials out there is refering to Express. I might change that later.
 See here the reason why you might want to use Restify: http://mcavage.github.com/node-restify/
 
 I also did a simple test with mysql: https://github.com/frodefi/node-mysql-json-server
 and mongodb and socket.io: https://github.com/frodefi/node-mongodb-socketio-server
 
-Please feel free to use this code as you like, MIT style :)
+This is currently a work in progress. My goal is to get it as production ready as possible.
+I will include some tips and links along the way.
+
+Please feel free to use the code as you like, MIT style :)
--- test/test.js
+++ test/test.js
@@ -1,18 +1,83 @@
-var util       = require('util');
-var assert     = require('assert');
-var DbProvider = require('../db').DbProvider;
-var dbProvider = new DbProvider('localhost', 27017, 'nki');
-
-// Just testing testing, much more to come...
+'use strict';
+// Really just testing testing, more to come...
 describe('DbProvider', function(){
-  describe('findAllNotes', function(){
-    it('should return some notes', function(done){
-      dbProvider.connect(function(){
-        dbProvider.findAllNotes({}, function (err, result){
-          assert(result.length > 0);
-          done();
-        });
+  var util       = require('util');
+  var assert     = require('assert');
+  var DbProvider = require('../db').DbProvider;
+  var dbProvider = new DbProvider('localhost', 27017, 'test');
+  var fixtures   = [
+    {
+      url: 'www.nytimes.com',
+      title: 'The New York Times',
+      description: 'Newspaper',
+      tags: 'news, "New York"'
+    },
+    {
+      url: 'digi.no',
+      title: 'Digi',
+      description: 'ITnyheter',
+      tags: 'news it norway'
+    }
+  ];
+  console.log("Array:" + Array.isArray(fixtures));
+  before(function(done){ // Connect to the test-database
+    dbProvider.connect(function(){
+      console.log('1');
+      done();
+    });
+  });
+  before(function(done){ // Make the note-collection empty
+    dbProvider.deleteNote({},function(){
+      console.log('1.4');
+      done();
+    });
+/*
+    var end = Date.now() + 1000;
+    while (Date.now() < end) ;
+    console.log('1.5');
+*/
+  });
+  before(function(done){
+    console.log('1.6.1:' + util.inspect(fixtures[0]));
+    dbProvider.saveNote(fixtures[0],function(){
+      done();
+    });
+  });
+  before(function(done){
+    console.log('1.6.1:' + util.inspect(fixtures[1]));
+    dbProvider.saveNote(fixtures[1],function(){
+      done();
+    });
+  });
+/*
+  for(var i in fixtures) {  // Insert data sample
+    console.log('1.6:' + i + ':' + util.inspect(fixtures[i]));
+    before(function(done){
+      console.log('1.6.5:' + i + ':' + util.inspect(fixtures[i]));
+      dbProvider.saveNote(fixtures[i],function(){
+        done();
       });
-    })
-  })
-})
+    });
+  }
+*/
+  describe('findAllNotes', function(){
+    it('should return some 0 notes', function(done){
+      console.log('2');
+      dbProvider.findAllNotes({}, function (err, result){
+        console.log('3');
+        assert.equal(result.length,2);
+        done();
+      });
+    });
+  });
+  describe('saveNote', function(){
+    it('should insert a note without error', function(done){
+      console.log('4');
+      dbProvider.saveNote({}, function (err, result){
+        console.log('5');
+        assert.equal(err,null);
+        done();
+      });
+    });
+  });
+});
